e7455573447c085686e41c135598d4528a6815fe
torlalinuxtoro <munford.ns@gmail.com>
2019-02-24 11:46:53 +0100

    fix script

--- test.sh
+++ test.sh
@@ -11,7 +11,6 @@ testsForCase="10"
 
 timePerTest="1000"
 graphSize="20"
-graphSize="20"
 minCost="1"
 maxCost="1000"
 popSize="100"
@@ -24,22 +23,21 @@ echo "starting script test"
 echo " " > $filePath
 
 
-sum=0
 
-echo -n  "0/$testsForCase"
-
-for steadyRatio in 0 0.1 0.5 0.7 0.9
+for steadyRatio in 0 0.1 0.5 0.7 0.9;
 do
-    echo -e "\b\b\b\b\b\b\bsteadyR=$steadyRatio"
-    echo -n "$steadyRatio," >> $filePath;
+    echo -e "\rsteadyR=$steadyRatio"
+    echo -n "0/$testsForCase"
+    echo -n "$steadyRatio," >> $filePath
+
+    sum=0
     for i in $(seq 1 $testsForCase);
     do
-        echo -n -e "\b\b\b\b\b\b\b\b\btest:$i/$testsForCase"
-        echo -n "$i," >> $filePath
-        value=$(./GeneticCompetitiveSalesMan --seed ${i} --time ${timePerTest} --graphSize ${graphSize} --graphMinCost ${minCost} --graphMaxCost ${maxCost} --popSize ${popSize} --mutRate 0.5 --steadyRatio 0.9 --nearRatio 0 --bottleNeckRatio 0 --disasterRate 0 |tr -d '\n ')
+        echo -n -e "\r$i/$testsForCase"
+        value=$(./GeneticCompetitiveSalesMan --seed ${i} --time ${timePerTest} --graphSize ${graphSize} --graphMinCost ${minCost} --graphMaxCost ${maxCost} --popSize ${popSize} --mutRate 0.5 --steadyRatio ${steadyRatio} --nearRatio 0 --bottleNeckRatio 0 --disasterRate 0 |tr -d '\n ')
         ((sum=sum+value))
     done
-    echo $((sum/testsForCase)) >> $filePath;
+    echo $((sum/testsForCase)) >> $filePath
 done
 
 
